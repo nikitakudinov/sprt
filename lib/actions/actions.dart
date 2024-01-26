f8f588edde817a572dbb55de84eca85883615868
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-26 19:32:02 +0000

    Updating to latest FlutterFlow output.

--- lib/actions/actions.dart
+++ lib/actions/actions.dart
@@ -1,4 +1,3 @@
-import '/auth/supabase_auth/auth_util.dart';
 import '/backend/api_requests/api_calls.dart';
 import '/backend/schema/structs/index.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
@@ -7,7 +6,6 @@ import 'package:flutter/material.dart';
 
 Future baseloader(BuildContext context) async {
   ApiCallResponse? updates;
-  ApiCallResponse? chatidarray;
   ApiCallResponse? chats;
 
   updates = await UpdatesGroup.updatesCall.call();
@@ -32,19 +30,7 @@ Future baseloader(BuildContext context) async {
       ),
     );
   } else {
-    chatidarray = await ChatsGroup.authchatmembersCall.call(
-      playerUid: currentUserUid,
-    );
-    chats = await ChatsGroup.chatsbyidsCall.call(
-      chatIds: valueOrDefault<String>(
-        ChatsGroup.authchatmembersCall
-            .chatid(
-              (chatidarray.jsonBody ?? ''),
-            )
-            ?.toString(),
-        '1',
-      ),
-    );
+    chats = await ChatsGroup.chatsCall.call();
     FFAppState().update(() {
       FFAppState().updateMAINDATAStruct(
         (e) => e
@@ -66,5 +52,15 @@ Future baseloader(BuildContext context) async {
           ),
       );
     });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text(
+          'chats loaded!',
+          style: TextStyle(),
+        ),
+        duration: const Duration(milliseconds: 1000),
+        backgroundColor: FlutterFlowTheme.of(context).tertiary,
+      ),
+    );
   }
 }

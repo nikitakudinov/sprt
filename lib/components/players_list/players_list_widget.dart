a029ca523da8740e81e8adbeeee702774b2f477b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-18 11:27:00 +0000

    Updating to latest FlutterFlow output.

--- lib/components/players_list/players_list_widget.dart
+++ lib/components/players_list/players_list_widget.dart
@@ -45,91 +45,107 @@ class _PlayersListWidgetState extends State<PlayersListWidget> {
   Widget build(BuildContext context) {
     context.watch<FFAppState>();
 
-    return Builder(
-      builder: (context) {
-        final players = widget.data!.toList();
-        return ListView.separated(
-          padding: EdgeInsets.zero,
-          scrollDirection: Axis.vertical,
-          itemCount: players.length,
-          separatorBuilder: (_, __) => const SizedBox(height: 5.0),
-          itemBuilder: (context, playersIndex) {
-            final playersItem = players[playersIndex];
-            return Container(
-              decoration: BoxDecoration(
-                color: FlutterFlowTheme.of(context).secondaryBackground,
-                borderRadius: BorderRadius.circular(5.0),
-                shape: BoxShape.rectangle,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Container(
-                      width: 40.0,
-                      height: 40.0,
-                      decoration: BoxDecoration(
-                        color: FlutterFlowTheme.of(context).secondaryBackground,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Container(
-                        width: 40.0,
-                        height: 40.0,
-                        clipBehavior: Clip.antiAlias,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        child: Image.asset(
-                          'assets/images/russia-flag-wallpaper-celebration-day_982005-4333.jpg',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: FlutterFlowTheme.of(context).primary,
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Builder(
+            builder: (context) {
+              final players = widget.data!.toList();
+              return ListView.separated(
+                padding: EdgeInsets.zero,
+                scrollDirection: Axis.vertical,
+                itemCount: players.length,
+                separatorBuilder: (_, __) => const SizedBox(height: 5.0),
+                itemBuilder: (context, playersIndex) {
+                  final playersItem = players[playersIndex];
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: FlutterFlowTheme.of(context).secondaryBackground,
+                      borderRadius: BorderRadius.circular(5.0),
+                      shape: BoxShape.rectangle,
                     ),
-                    Expanded(
-                      child: Column(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
                         mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            valueOrDefault<String>(
-                              playersItem.nickname,
-                              'Nickname',
+                          Container(
+                            width: 40.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                              color: FlutterFlowTheme.of(context)
+                                  .primaryBackground,
+                              shape: BoxShape.circle,
                             ),
-                            style: FlutterFlowTheme.of(context).titleSmall,
+                            child: Container(
+                              width: 40.0,
+                              height: 40.0,
+                              clipBehavior: Clip.antiAlias,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                              ),
+                              child: Image.asset(
+                                'assets/images/russia-flag-wallpaper-celebration-day_982005-4333.jpg',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                           ),
-                          Text(
-                            playersItem.uid,
-                            style: FlutterFlowTheme.of(context).labelSmall,
+                          Expanded(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  valueOrDefault<String>(
+                                    playersItem.nickname,
+                                    'Nickname',
+                                  ),
+                                  style:
+                                      FlutterFlowTheme.of(context).titleSmall,
+                                ),
+                                Text(
+                                  playersItem.uid,
+                                  style:
+                                      FlutterFlowTheme.of(context).labelSmall,
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              FlutterFlowIconButton(
+                                borderRadius: 20.0,
+                                borderWidth: 1.0,
+                                buttonSize: 40.0,
+                                icon: Icon(
+                                  Icons.chat_bubble_rounded,
+                                  color:
+                                      FlutterFlowTheme.of(context).primaryText,
+                                  size: 24.0,
+                                ),
+                                onPressed: () {
+                                  print('IconButton pressed ...');
+                                },
+                              ),
+                            ],
+                          ),
+                        ].divide(const SizedBox(width: 10.0)),
                       ),
                     ),
-                    Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        FlutterFlowIconButton(
-                          borderRadius: 20.0,
-                          borderWidth: 1.0,
-                          buttonSize: 40.0,
-                          icon: Icon(
-                            Icons.chat_bubble_rounded,
-                            color: FlutterFlowTheme.of(context).primaryText,
-                            size: 24.0,
-                          ),
-                          onPressed: () {
-                            print('IconButton pressed ...');
-                          },
-                        ),
-                      ],
-                    ),
-                  ].divide(const SizedBox(width: 10.0)),
-                ),
-              ),
-            );
-          },
-        );
-      },
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }

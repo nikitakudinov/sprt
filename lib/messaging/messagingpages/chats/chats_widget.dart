f42da504122befde196bb951434bbdd3b947d4c4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-18 11:31:58 +0000

    Updating to latest FlutterFlow output.

--- lib/messaging/messagingpages/chats/chats_widget.dart
+++ lib/messaging/messagingpages/chats/chats_widget.dart
@@ -160,6 +160,7 @@ class _ChatsWidgetState extends State<ChatsWidget> {
                         0,
                         0,
                       ),
+                      reverse: true,
                       shrinkWrap: true,
                       scrollDirection: Axis.vertical,
                       itemCount: chats.length,
